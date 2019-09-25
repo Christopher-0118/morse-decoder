0811859bc38f64028f39b3204a061d985506fb68
Kristina Karantsevich <k.karantsevich@gmail.com>
2019-09-25 22:50:46 +0300

    init: add function that forms words

--- src/index.js
+++ src/index.js
@@ -38,13 +38,44 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    
+  let dividedString = divideString (expr, 2);
+  let morseCode = convertToMorse (dividedString);
 }
 
-function divideString (expr) {
-    let dividedString = [];
+function divideString (expr, step) {
+  let dividedString = [];
+  let stringLength = (expr.length - 1);
+  let startSubSrting = 0;
+  let endSubString = step;  
 
-    
+  while (endSubString <= stringLength) {
+    dividedString.push(expr.slice(startSubSrting, endSubString));
+    startSubSrting += step;
+    endSubString = startSubSrting + step;
+  }
+  return dividedString;
+}
+
+function convertToMorse (dividedString) { // the function create word, but you need letters
+  let convertedToMorse = [];
+  let word = '';
+  let arrayLength = (dividedString.length - 1);
+
+  for (let i = 0; i <= arrayLength; i++) {
+    if (dividedString[i] === '10') {
+      word += '.';
+    }
+    else if (dividedString[i] === '11') {
+      word += '-';
+    }
+    else if (dividedString[i] === '**' && dividedString[i + 1] !== '**') {
+      convertedToMorse.push(word);
+      convertedToMorse.push(' ');
+      word = '';
+    }
+  }
+  convertedToMorse.push(word);
+  return convertedToMorse;
 }
 
 module.exports = {
