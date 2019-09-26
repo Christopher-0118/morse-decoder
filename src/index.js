52c98e100c688f8a39a7d6a2639041095db9946d
Kristina Karantsevich <k.karantsevich@gmail.com>
2019-09-27 00:05:48 +0300

    feat: convertToMorse function changed and decode function was done

--- src/index.js
+++ src/index.js
@@ -1,50 +1,66 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+  '.-':     'a',
+  '-...':   'b',
+  '-.-.':   'c',
+  '-..':    'd',
+  '.':      'e',
+  '..-.':   'f',
+  '--.':    'g',
+  '....':   'h',
+  '..':     'i',
+  '.---':   'j',
+  '-.-':    'k',
+  '.-..':   'l',
+  '--':     'm',
+  '-.':     'n',
+  '---':    'o',
+  '.--.':   'p',
+  '--.-':   'q',
+  '.-.':    'r',
+  '...':    's',
+  '-':      't',
+  '..-':    'u',
+  '...-':   'v',
+  '.--':    'w',
+  '-..-':   'x',
+  '-.--':   'y',
+  '--..':   'z',
+  '.----':  '1',
+  '..---':  '2',
+  '...--':  '3',
+  '....-':  '4',
+  '.....':  '5',
+  '-....':  '6',
+  '--...':  '7',
+  '---..':  '8',
+  '----.':  '9',
+  '-----':  '0',
 };
 
 function decode(expr) {
-  let dividedString = divideString (expr, 2);
-  let morseCode = convertToMorse (dividedString);
+  let dividedString = divideString(expr, 10);
+  let arrayOfLetters = [];
+  let morseCode = [];
+  let decodedString = [];
+
+  for (let i = 0; i < dividedString.length; i++) {
+    arrayOfLetters.push(divideString(dividedString[i], 2));
+  }
+  for (let i = 0; i < arrayOfLetters.length; i++) {
+    morseCode.push(convertToMorse(arrayOfLetters[i]));
+  }
+  for (let i = 0; i < morseCode.length; i++) {
+    if (morseCode[i] == ' ') {
+      decodedString.push(' ');
+    }
+    decodedString.push(MORSE_TABLE[morseCode[i]]);
+  }
+  return decodedString.join('');
 }
 
-function divideString (expr, step) {
+function divideString(expr, step) {
   let dividedString = [];
-  let stringLength = (expr.length - 1);
+  let stringLength = expr.length;
   let startSubSrting = 0;
   let endSubString = step;  
 
@@ -56,25 +72,24 @@ function divideString (expr, step) {
   return dividedString;
 }
 
-function convertToMorse (dividedString) { // the function create word, but you need letters
+function convertToMorse(dividedString) {
   let convertedToMorse = [];
-  let word = '';
-  let arrayLength = (dividedString.length - 1);
+  let letter = '';
+  let arrayLength = dividedString.length - 1;
 
   for (let i = 0; i <= arrayLength; i++) {
-    if (dividedString[i] === '10') {
-      word += '.';
+    if (dividedString[i] == '10') {
+      letter += '.';
     }
-    else if (dividedString[i] === '11') {
-      word += '-';
+    else if (dividedString[i] == '11') {
+      letter += '-';
     }
-    else if (dividedString[i] === '**' && dividedString[i + 1] !== '**') {
-      convertedToMorse.push(word);
+    else if (dividedString[i] == '**') {
       convertedToMorse.push(' ');
-      word = '';
+      return convertedToMorse;
     }
   }
-  convertedToMorse.push(word);
+  convertedToMorse.push(letter);
   return convertedToMorse;
 }
 
